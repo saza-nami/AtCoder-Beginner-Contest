3fdccd396e36705ed10161dfbb3d4a6278e8b295
saza-nami <saza-nami@users.noreply.github.com>
2022-06-10 13:07:19 +0900

    ABC043 A AC

---
+++ A - キャンディーとN人の子供イージー.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+  int N;
+  int Candy = 0;
+  scanf("%d", &N);
+  for(int i = 1; i <= N; i++){
+  	Candy += i;
+  }
+  printf("%d", Candy);
+  return 0;
+}
